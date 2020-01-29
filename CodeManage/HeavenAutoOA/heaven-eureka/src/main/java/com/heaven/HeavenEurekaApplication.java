beb598f3e27ad316f7430f06630ae93f0deb4e74
heiheihuang <1245026448@qq.com>
2020-01-29 12:23:08 +0800

    【Heaven】对Eureka调用接口增加security保护

--- CodeManage/HeavenAutoOA/heaven-eureka/src/main/java/com/heaven/HeavenEurekaApplication.java
+++ CodeManage/HeavenAutoOA/heaven-eureka/src/main/java/com/heaven/HeavenEurekaApplication.java
@@ -3,9 +3,11 @@ package com.heaven;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaServer
+@ComponentScan(basePackages="com.heaven.*")
 public class HeavenEurekaApplication {
 
     public static void main(String[] args) {
